b5bed3e7bb427b5f5bf825480dc60ee87dc7a484
matthew-collett <97645707+matthew-collett@users.noreply.github.com>
2023-03-13 22:18:38 -0300

    Slight text adjustments and swiper

--- public/scripts/swiper.js
+++ public/scripts/swiper.js
@@ -1,19 +1,34 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
     slidesPerView: 3,
     slidesPerGroup: 1,
     spaceBetween: 20,
     scrollDirection: 'horizontal',
     simulateTouch: true,
+    loopFillGroupWithBlank: true,
+    loop: true,
+    loopedSlides: 4,
+    slideToClickedSlide: true,
 
-    // If we need pagination
     pagination: {
         el: '.swiper-pagination',
     },
 
-    // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1
+        },
+
+        696: {
+            slidesPerView: 2
+        },
+
+        1030: {
+            slidesPerView: 3
+        }
+    }
   });
